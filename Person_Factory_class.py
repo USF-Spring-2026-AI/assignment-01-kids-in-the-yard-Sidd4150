5905f107fc93374a9002bc1e8eb963b5665a8967
Siddhartha Shakya <sshakya@dons.usfca.edu>
2026-02-11 22:53:24 -0800

    added loop with a queue to generate all children

--- Person_Factory_class.py
+++ Person_Factory_class.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import random
 import csv
-from collections import defaultdict
+from collections import defaultdict, deque
 import math
 from Person_class import Person
 
@@ -61,58 +61,89 @@ class PersonFactory():
             decade_key = self.get_decade(year)
             print(decade_key)
             
-            number_kids = self.chance_kids(decade_key)
+            number_kids,_ = self.chance_kids(decade_key)
             print(number_kids)
 
             for i in range(number_kids):
-                self.createChildren(Molly_Jones.age,Molly_Jones)
+                self.createChildren(Molly_Jones.year_born,Molly_Jones)
+
+            Desmond_Jones.children =  Molly_Jones.children
+
         else:
             year = Desmond_Jones.year_born
             decade_key = self.get_decade(year)
             print(decade_key)
             
-            number_kids = self.chance_kids(decade_key)
-            print(number_kids)
+            number_kids,_ = self.chance_kids(decade_key)
+            print("Number of kids", number_kids)
 
             for i in range(number_kids):
-                self.createChildren(Desmond_Jones.age,Desmond_Jones)
-                
-           
+                self.createChildren(Desmond_Jones.year_born,Desmond_Jones)
+            
+            Molly_Jones.children = Desmond_Jones.children
+
+        #Tree Building
+        # Looked up built in python queue found at https://www.geeksforgeeks.org/python/queue-in-python/
+        queue = deque()
+        
+        for child in Molly_Jones.children:
+            queue.append(child)
+
+        while queue:
+            curr_person = queue.popleft()
+            kids, is_married = self.chance_kids(self.get_decade(curr_person.year_born))
+
+            if is_married == "marry":
+                curr_person.partner = self.createChildren(curr_person.year_born + random.randint(-10,10), None)
+                curr_person.partner.partner = curr_person
+
+         
 
           
     def createChildren(self,Elder_Age,Parent):
         #Desmond_Jones = Person(1950, Desmond_death, "Desmond", "Jones", None, None, Desmond_age)
-        year_born = Parent.year_born + random.randint(25,45)
-        death = float(self.lifeProb[str(year_born)]) + random.randint(-10,10)
+        if Parent == None:
+            year_born = Elder_Age
+        else:
+            year_born = math.ceil(Elder_Age) + random.randint(25,45) 
+        
+        death = year_born + float(self.lifeProb[str(year_born)])+ random.randint(-10,10)
         
         decade_key = self.get_decade(year_born)
-        
+      
         gender = self.get_gender(decade_key)
         first_name = self.first_gen(decade_key,gender)
         
         New_Child = Person(
             year_born,
             death,
-            #Need to create random name logic
+           
             first_name,
-            Parent.lastN,
+            Parent.lastN if Parent else "LASTNAME",
             None,
             None,
             death-year_born,       
         )
-        Parent.children.append(New_Child)
+
+        if Parent != None:
+            Parent.children.append(New_Child)
+        
+        return New_Child
         
     def chance_kids(self,decade):
-        birth_rate = float(self.birth_marriage[decade][0])
+       
+        marriage_chance =  float(self.birth_marriage[decade][1])
+        is_married = random.choices(["marry", "single"], weights=[marriage_chance, 1-marriage_chance], k=1)[0]
 
+        birth_rate = float(self.birth_marriage[decade][0])
         number_kids = math.ceil(birth_rate + random.uniform(-1.5,1.5))
-        return number_kids
+        return number_kids, is_married
     
     def get_gender(self, decade):
         gender_vals = self.genderProb[decade]
         
         
-        genders = [item[0] for item in gender_vals] # ['male', 'female']
+        genders = [item[0] for item in gender_vals]
         freq = [item[1] for item in gender_vals]
         
         gender = random.choices(genders, weights=freq, k=1)[0]
