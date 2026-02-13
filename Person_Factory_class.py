3a1441a5c59a3839a31cda4b133957b2b745059b
Siddhartha Shakya <sshakya@dons.usfca.edu>
2026-02-13 14:21:13 -0800

    fixing logic errors and formatting

--- Person_Factory_class.py
+++ Person_Factory_class.py
@@ -64,7 +64,7 @@ class PersonFactory():
             number_kids,_ = self.chance_kids(decade_key)
             print(number_kids)
 
-            for i in range(number_kids):
+            for _ in range(number_kids):
                 self.createChildren(Molly_Jones.year_born,Molly_Jones)
 
             Desmond_Jones.children =  Molly_Jones.children
@@ -77,7 +77,7 @@ class PersonFactory():
             number_kids,_ = self.chance_kids(decade_key)
             print("Number of kids", number_kids)
 
-            for i in range(number_kids):
+            for _ in range(number_kids):
                 self.createChildren(Desmond_Jones.year_born,Desmond_Jones)
             
             Molly_Jones.children = Desmond_Jones.children
@@ -106,33 +106,35 @@ class PersonFactory():
                 # print("Partners Death:", curr_person.partner.death)
                 # print("Partners Age:", curr_person.partner.age)
 
-                if curr_person.age > curr_person.partner.age:
+                if curr_person.age < curr_person.partner.age:
                     number_kids,_ = self.chance_kids(self.get_decade(curr_person.year_born))
-                    for i in range(number_kids):
+                    for _ in range(number_kids):
                         new_child = self.createChildren(curr_person.year_born,curr_person)
 
                         if new_child == None:
                             continue
-                        curr_person.partner.children = curr_person.children
+                        
                         print("Married:", new_child.year_born, new_child.firstN)
                         #for child in curr_person.children:
-                        
                         queue.append(new_child)
-                elif curr_person.age < curr_person.partner.age:
+                    curr_person.partner.children = curr_person.children
+
+                elif curr_person.age >= curr_person.partner.age:
                     number_kids,_ = self.chance_kids(self.get_decade(curr_person.partner.year_born))
-                    for i in range(number_kids):
+                    for _ in range(number_kids):
                         new_child = self.createChildren(curr_person.partner.year_born,curr_person.partner)
 
                         if new_child == None:
                             break
-                        curr_person.children = curr_person.partner.children
+                       
                         print("Married:", new_child.year_born, new_child.firstN)
                         #for child in curr_person.partner.children:
-                        
                         queue.append(new_child)
+                    curr_person.children = curr_person.partner.children
+
             else:
                 number_kids, _ = self.chance_kids(self.get_decade(curr_person.year_born))
-                for i in range(number_kids):
+                for _ in range(number_kids):
                     new_child = self.createChildren(curr_person.year_born,curr_person)
 
                     if new_child == None:
@@ -141,36 +143,27 @@ class PersonFactory():
                     
                     queue.append(new_child)
 
-     
-
-                        
-                        
-            
-
-
-
           
     def createChildren(self,Elder_Age,Parent):
         #Desmond_Jones = Person(1950, Desmond_death, "Desmond", "Jones", None, None, Desmond_age)
         if Parent == None:
-            year_born = Elder_Age
+            year_born = Elder_Age +  random.randint(-5,5)
         else:
             year_born = math.ceil(Elder_Age) + random.randint(25,45) 
         if year_born > 2120:
             return None
-        death = year_born + float(self.lifeProb[str(year_born)])+ random.randint(-10,10)
         
+        death = year_born + float(self.lifeProb[str(year_born)])+ random.randint(-10,10) 
         decade_key = self.get_decade(year_born)
-      
         gender = self.get_gender(decade_key)
         first_name = self.first_gen(decade_key,gender)
-        
+        random_last_name = self.get_random_last_name(decade_key)
+
         New_Child = Person(
             year_born,
             death,
-           
             first_name,
-            Parent.lastN if Parent else "LASTNAME",
+            Parent.lastN if Parent else random_last_name,
             None,
             None,
             death-year_born,       
@@ -207,62 +200,51 @@ class PersonFactory():
         return decade_key
     
     def first_gen(self, decade,gender):
-        
-     
-        
+             
         gendered_options = [val for val in self.firstNames[decade] if val[0] == gender]
    
         first_names = [val[1] for val in gendered_options]
-
-    
         freq = [val[2] for val in gendered_options]
 
 
         name = random.choices(first_names, weights=freq, k=1)[0]
         return name
         
+    def get_random_last_name(self,decade):
+        lastNames = self.lastNames[decade]
         
+        
+        list_last_names = [item[1] for item in lastNames]
+
+        random.shuffle(list_last_names)
+        name = list_last_names.pop()
+        return name
     
-    def get_files(self):
-        
-        #create a map with {key=year: val= (birth,marriage)}
-        with open('birth_and_marriage_rates.csv', mode='r') as infile:
+    def load_csv(self, filename):
+        data = []
+        with open(filename, mode='r') as infile:
             reader = csv.reader(infile)
-            #skip header
-            next(reader)
-            for rows in reader:
-                self.birth_marriage[rows[0]] = (float(rows[1]), float(rows[2]))
-                
-        
-        with open('first_names.csv', mode='r') as infile:
-            reader = csv.reader(infile)
-            #skip header
-            next(reader)
-            for rows in reader:
-                self.firstNames[rows[0]].append((rows[1],rows[2],float(rows[3])))
-                
-        with open('gender_name_probability.csv', mode='r') as infile:
-            reader = csv.reader(infile)
-            #skip header
-            next(reader)
-            for rows in reader:
-                self.genderProb[rows[0]].append((rows[1],float(rows[2])))
-                                            
-        with open('last_names.csv', mode='r') as infile:
-            reader = csv.reader(infile)
-            #skip header
-            next(reader)
-            for rows in reader:
-                self.lastNames[rows[0]].append((rows[1],rows[2]))
-                
-                                            
-        with open('life_expectancy.csv', mode='r') as infile:
-            reader = csv.reader(infile)
-            #skip header
-            next(reader)
-            for rows in reader:
-                self.lifeProb[rows[0]] = (rows[1]) 
-                
-        
+            next(reader) # Skip header
+            for row in reader:
+                data.append(row)
+        return data
+    
+    def get_files(self):   
+        #create a map with {key=year: val=rest of the data}
+        for rows in self.load_csv('birth_and_marriage_rates.csv'):
+            self.birth_marriage[rows[0]] = (float(rows[1]), float(rows[2]))
             
-   
+        for rows in self.load_csv('first_names.csv'):
+            self.firstNames[rows[0]].append((rows[1],rows[2],float(rows[3])))
+        
+        for rows in self.load_csv('gender_name_probability.csv'):
+            self.genderProb[rows[0]].append((rows[1],float(rows[2])))
+                                             
+        for rows in self.load_csv('last_names.csv'):
+            self.lastNames[rows[0]].append((rows[1],rows[2]))
+                                        
+        for rows in self.load_csv('life_expectancy.csv'):
+            self.lifeProb[rows[0]] = (rows[1]) 
+            
+        
+  
